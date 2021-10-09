551bc8f99b6dfe85bb4414aeb56d11ee80bb0ce0
Kostiantyn Konovalenko <kostiantyn.konovalenko@cyper.ca>
2021-10-09 23:21:33 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import style from './Style.module.scss'
 
 export  const  App = () => {
   return (
+      <div>PLEASE SEE THE CODE</div>
     // DON'T WORK BECAUSE JAVASCRIPT DON'T UNDERSTAND "-" in variables  
     // <div className={style.hello-world}>Hello</div>
     // It's good work 
     // <div className={style["hello-world"]}>Hello</div>
     // <div className={style.helloWorld}>Hello</div>
     // <div className={style.hello_world}>Hello</div>
-    null
+
     )
 }
